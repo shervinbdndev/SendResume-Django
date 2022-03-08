990e8c83fe47eae4c3711beefba7f26deb645b43
shervin-glitch <shervin2234@gmail.com>
2022-03-08 09:17:08 -0800

    Commited

--- sendinformationform/urls.py
+++ sendinformationform/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
+if (settings.DEBUG):
+    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
